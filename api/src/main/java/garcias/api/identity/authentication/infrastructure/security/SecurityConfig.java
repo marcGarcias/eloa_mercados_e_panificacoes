b4536b7685a88a0efad2500ad9533492b7512552
Marcelo Garcias <garcias.marcelo98@proton.me>
2026-08-30 14:35:35 -0300

    fix(api): corrige CORS 403 no preflight OPTIONS liberando wildcards da vercel

--- api/src/main/java/garcias/api/identity/authentication/infrastructure/security/SecurityConfig.java
+++ api/src/main/java/garcias/api/identity/authentication/infrastructure/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package garcias.api.identity.authentication.infrastructure.security;
 
-import garcias.api.identity.authentication.infrastructure.security.jwt.JwtAuthenticationFilter;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
+
+import garcias.api.identity.authentication.infrastructure.security.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
